edc13dba5afb83561520e04c03b7deb33f757a11
bysyangat <151016855+bysyangat@users.noreply.github.com>
2024-08-16 18:02:03 +0800

    Update taskmanager.py

--- taskmanager.py
+++ taskmanager.py
@@ -1,2 +1,24 @@
+import datetime
+from task import Task
+
 class TaskManager:
-    pass
+    def __init__(self):
+        self.tasks = []
+    
+    def add_task(self, task):
+        '''
+        Add a task to the list
+        '''
+        # the append method adds an item in a list
+        self.tasks.append(task)
+    
+    def edit_task(self, item, edited_task):
+        '''
+        Edit a task
+        '''
+        # checks if the input for 'item' is an integer
+        if item.isdigit(item):
+            # this updates a specific item in a list
+            self.tasks[item] = edited_task
+        # shows the user that 'item' input is not an integer
+        else: print("Please enter a number for item."
