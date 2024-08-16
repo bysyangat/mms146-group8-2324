48bcc60d77af9a57745df3b1fc19499c120155f9
Bambi1118 <151016932+Bambi1118@users.noreply.github.com>
2024-08-16 10:17:14 +0800

    Update task.py
    rose vertucio

--- task.py
+++ task.py
@@ -1,2 +1,6 @@
 class Task:
     pass
+    
+    def __init__(self, priority_level="low", completion_status=0):
+        self.priority_level = priority_level
+        self.completion_status = completion_status
