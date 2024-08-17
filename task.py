81bc75d9caffdaec4fcfa69295d2f48f1153db47
SophiaKaye <sbcendana@up.edu.ph>
2024-08-17 20:30:03 +0800

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,9 @@
 class Task:
     pass
-    
-    def __init__(self, priority_level="low", completion_status=0):
+
+    def __init__(self, task_name, description, due_date, priority_level="low", completion_status=0):
+        self.task_name = task_name
+        self.description = description
+        self.due_date = due_date
         self.priority_level = priority_level
         self.completion_status = completion_status
