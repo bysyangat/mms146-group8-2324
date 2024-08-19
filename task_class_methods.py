54d11d6b8075fb427f2afe44dab3d7a44a3cf81a
CatPL8 <143311157+CatPL8@users.noreply.github.com>
2024-08-19 21:33:37 +0800

    Add files via upload

---
+++ task_class_methods.py
@@ -0,0 +1,11 @@
+
+
+
+  def set_description(self, description):
+      self.description = description
+
+  def set_priority(self, priority):
+      self.priority_level = priority
+
+  def set_completion_status(self, status):
+      self.completion_status = status
