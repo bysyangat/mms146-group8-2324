2f26daf5547e1392ffa83723ddaf0a761ed5abf4
Zhoushie13 <jabiantan@up.edu.ph>
2024-08-20 04:22:19 +0800

    Update taskmanager.py

--- taskmanager.py
+++ taskmanager.py
@@ -22,3 +22,17 @@ class TaskManager:
             self.tasks[item] = edited_task
         # shows the user that 'item' input is not an integer
         else: print("Please enter a number for item.")
+
+
+    def get_overdue_tasks(self):
+    '''
+    Show tasks that are overdue
+    '''
+    now = datetime.datetime.now()
+    overdue_tasks = []
+    
+    for task in self.tasks:
+        if task.due_date < now and task.completion_status < 100:
+            overdue_tasks.append(task)
+    
+    return overdue_tasks
